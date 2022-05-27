396dda126936fa755eedcd89633ee4a197cb7be5
Dean Giroir <deang1983@hotmail.com>
2022-05-26 19:24:40 -0500

    Animation adds

--- js/index.js
+++ js/index.js
@@ -10,3 +10,22 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+// ANIMATIONS
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 20;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
